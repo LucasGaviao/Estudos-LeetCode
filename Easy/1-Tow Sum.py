c70f1db551de05a262165ab8d23b41bfa4a7d52b
Lucas Gaviao <143828910+LucasGaviao@users.noreply.github.com>
2024-09-07 22:47:31 -0300

    Create 1-Tow Sum.py

---
+++ Easy/1-Tow Sum.py
@@ -0,0 +1,8 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        hashmap = {}
+        for i in range(len(nums)):
+            a = target - nums[i]
+            if a in hashmap:
+                return[i, hashmap[a]]
+            hashmap[nums[i]] = i
